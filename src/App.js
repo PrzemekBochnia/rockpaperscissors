baadacc84ef15f48fbe87cf1a0afca1990d4ecdb
Przemek587 <72383546+Przemek587@users.noreply.github.com>
2022-05-17 09:50:57 +0200

    add all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import background from "./assets/background.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=> {
+
+  const[userChoice, setUserChoice] = useState(null)
+  const[computerChoice, setComputerChoice] = useState(null)
+  const[result, setResult] = useState(null)
+
+  const choices = ['rock', 'paper', 'scissors']
+
+  const handleClick = (value) =>{
+    setUserChoice(value)
+    generateComputerChoice()
+  }
+  
+  const generateComputerChoice = () =>{
+    const randomChoice = choices[Math.floor( Math.random() * choices.length)]
+    setComputerChoice(randomChoice)
+  }
+  useEffect(()=>{
+    checkResult()
+  },[computerChoice,userChoice])
+
+  const checkResult = () =>{
+    switch(userChoice+computerChoice){
+      case 'rockscissors':
+      case 'scissorspaper':
+      case 'paperrock':
+        setResult('You Win!')
+        break
+      case 'scissorsrock':
+      case 'paperscissors':
+      case 'rockpaper':
+        setResult('You loose...')
+        break
+      case 'scissorsscissors':
+      case 'rockrock':
+      case 'paperpaper':
+        setResult('It,s a draw')
+        break                
+    }
+  }
+  const winStyle={
+    color:'green'
+  }
+  const looseStyle={
+    color:'red'
+  }
+  
+  if(result==='You Win!'){
+    return (
+      <div className="gameArea" style={{backgroundImage: `url(${background})`}}>
+      <h1 className="userChoice">User choice is: {userChoice} </h1>
+      <h1>Computer choice is: {computerChoice} </h1>
+      {choices.map((choice,index)=>
+      <button className="btn" key={index} onClick={()=>handleClick(choice)}>{choice}</button>
+      )}
+      <h1 style={winStyle}>{result}</h1>
     </div>
-  );
+  )
+}if(result==='You loose...'){
+  return(
+    <div className="gameArea" style={{backgroundImage: `url(${background})`}}>
+      <h1 className="userChoice">User choice is: {userChoice} </h1>
+      <h1>Computer choice is: {computerChoice} </h1>
+      {choices.map((choice,index)=>
+      <button className="btn" key={index} onClick={()=>handleClick(choice)}>{choice}</button>
+      )}
+      <h1 style={looseStyle}>{result}</h1>
+    </div>
+  )
+}else{
+  return(
+    <div className="gameArea" style={{backgroundImage: `url(${background})`}}>
+      <h1 className="userChoice">User choice is: {userChoice} </h1>
+      <h1>Computer choice is: {computerChoice} </h1>
+      {choices.map((choice,index)=>
+      <button className="btn" key={index} onClick={()=>handleClick(choice)}>{choice}</button>
+      )}
+      <h1>{result}</h1>
+    </div>
+  )
 }
+};
 
 export default App;
